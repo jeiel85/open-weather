3b44b0f21284570874b685551e3fdf223c3cde9f
박용은 <jeiel85@gmail.com>
2026-04-22 17:26:34 +0900

    fix: AndroidX 호환성 및 Java 21 빌드 환경 최적화 (v1.2.1 빌드 재시도)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,13 +11,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     signingConfigs {
@@ -34,7 +34,7 @@ android {
         applicationId = "com.jeiel.zephyr_sky"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
